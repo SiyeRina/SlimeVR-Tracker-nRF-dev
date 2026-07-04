5a582b48b198cfaa7a1ab6acc19520cdc1065bed
SiyeRina <1587756860@qq.com>
2026-07-04 19:25:53 +0800

    Fix stack overflow: increase sensor_init_thread stack from 256 to 1024 bytes (Zephyr fatal error reason=2)

--- src/sensor/sensor.c
+++ src/sensor/sensor.c
@@ -1,4 +1,4 @@
-﻿/*
+/*
 	SlimeVR Code is placed under the MIT license
 	Copyright (c) 2025 SlimeVR Contributors
 
@@ -295,8 +295,8 @@ static void sensor_update_range_stats_accel(float a[3]);
 static struct k_thread sensor_thread_id;
 static K_THREAD_STACK_DEFINE(sensor_thread_id_stack, 2048);
 
-K_THREAD_DEFINE(sensor_init_thread_id, 256, sensor_request_scan, true, NULL, NULL, 7, 0, 0);
-//crashing on nrf54l at 256
+K_THREAD_DEFINE(sensor_init_thread_id, 1024, sensor_request_scan, true, NULL, NULL, 7, 0, 0);
+// 256 was too small - stack overflow on nRF52840 during sensor init
 
 /* init thread handles starting scanner on the main thread, and then switches to the loop, before returning
    afterwards, other calls to start scanner will stop the loop on their thread and start the scanner on its own; it will also wait for the scanner to finish
